65646d7eab250ae33c9a2b26b4af62ecca7868ad
nikoladz1 <dzenopoljac94@gmail.com>
2022-09-03 20:24:22 +0200

    ss

---
+++ studsluz/src/studsluz/Admin.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studsluz;
+
+/**
+ *
+ * @author dzeno
+ */
+public class Admin extends Person{
+    private String username;
+    private String password;
+
+    public Admin(String username, String password, String name, String surname, String email) {
+        super(name, surname, email);
+        this.username = username;
+        this.password = password;
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ studsluz/src/studsluz/Person.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studsluz;
+
+/**
+ *
+ * @author dzeno
+ */
+public class Person {
+    
+    private String name;
+    private String surname;
+    private String email;
+
+    public Person(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public Person() {
+        this.name = "empty";
+        this.surname = "empty";
+        this.email = "empty";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
