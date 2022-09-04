7f9a023a6f1df68a8434e70c19057f751300ea1b
nikoladz1 <dzenopoljac94@gmail.com>
2022-09-04 20:23:21 +0200

    ss

--- studsluz/src/studsluz/Studsluz.java
+++ studsluz/src/studsluz/Studsluz.java
@@ -4,6 +4,9 @@
  */
 package studsluz;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author dzeno
@@ -16,8 +19,34 @@ public class Studsluz {
     public static void main(String[] args) {
         GreetMsg msg = new GreetMsg();
         msg.WelcomeMessage();
-        int ChosenOption;
+        int chosenOption =-1;
         
+        
+        while(chosenOption != 0 && chosenOption != 1){
+            Scanner scann = new Scanner(System.in);
+            try{
+                chosenOption = Integer.parseInt(scann.next()) ;
+                
+            }
+            
+            catch(NumberFormatException e){
+                System.out.println("Uneli ste nedozvoljeni karakter, pokusajte ponovo.");
+                continue;
+            }
+      
+            
+       
+        }
+        
+        if(chosenOption ==0){
+            System.out.println("Izlazak iz programa");
+            System.exit(0);
+        }
+        
+        if(chosenOption == 1){
+            chosenOption = -1;
+            msg.LogInMsg();
+        }
         
     }
     
